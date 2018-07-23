e0076fda0d5882aa111713fa7a49b14a6edd4857
Brian Cui <brian.cui@live.com>
2018-07-23 16:48:18 -0700

    next golden hour now displayed

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,56 @@
-import * as moment from "moment";
 import * as React from 'react';
 import './App.css';
-
 import getLocation from './dom/location';
-import getTimes from './lib/goldenHour';
+import getUpcomingGoldenHours from "./lib/goldenHour";
 import logo from './logo.svg';
 
 interface IAppState {
-  location: string;
+    location: string;
 }
 
 class App extends React.Component<object, IAppState> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      location: "Determining your location..."
-    };
-  }
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            location: "Determining your location..."
+        };
+    }
 
-  public render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, World!</h1>
-        </header>
-        <p className="App-intro">
-          {this.state.location}
-        </p>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Hello, World!</h1>
+                </header>
+                <p className="App-intro">
+                    {this.state.location}
+                </p>
+            </div>
+        );
+    }
 
-  public componentDidMount() {
-    getLocation()
-      .then(getTimes)
-      .then(times => {
-        const goldenHourString = moment(times.goldenHour).format("MMMM Do YYYY, h:mm:ss a")
-        this.setState({
-          location: `Golden hour is at: ${goldenHourString}`
-        })
-      })
-      .catch((error: string) => {
-        this.setState({
-          location: error,
-        });
-    });
-  }
+    public componentDidMount() {
+        getLocation()
+            .then(getUpcomingGoldenHours)
+            .then(goldenHours => {
+                let location: string;
+                if (goldenHours.morning.isBefore(goldenHours.evening)) {
+                    location = `Upcoming Morning: ${goldenHours.morning.format("MMMM Do YYYY, h:mm:ss a")}, Evening: ${goldenHours.evening.format("MMMM Do YYYY, h:mm:ss a")}`;
+                } else {
+                    location = `Upcoming Evening: ${goldenHours.evening.format("MMMM Do YYYY, h:mm:ss a")}, Next Morning: ${goldenHours.morning.format("MMMM Do YYYY, h:mm:ss a")}`;
+                }
+
+                this.setState({
+                    location,
+                });
+            })
+            .catch((error: string) => {
+                this.setState({
+                    location: error,
+                });
+            });
+    }
 }
 
 export default App;
--- src/lib/goldenHour.ts
+++ src/lib/goldenHour.ts
@@ -1,6 +1,50 @@
+import * as moment from "moment";
+import { Moment } from "moment";
 import * as SunCalc from "suncalc";
+
 import { IPosition } from "../dom/location";
 
-export default function getTimes(location: IPosition) {
-    return SunCalc.getTimes(new Date(), location.latitude, location.longitude);
+export interface IGoldenHours {
+    morning: Moment,
+    evening: Moment,
+}
+
+// tslint:disable:object-literal-sort-keys
+/**
+ * Gets the morning and evening golden hour times for a given day.
+ * @param date Specifies the current day.
+ * @param location Specifies the target location.
+ */
+function getGoldenHours(date: Moment, location: IPosition): IGoldenHours {
+    const times = SunCalc.getTimes(date.toDate(), location.latitude, location.longitude);
+
+    return {
+        morning: moment(times.goldenHourEnd),
+        evening: moment(times.goldenHour),
+    };
+}
+
+/**
+ * Gets the upcoming morning and evening golden hour times for the current time.
+ * If a golden hour is currently ongoing, it will be included in the result.
+ * @param location Specifies the target location.
+ */
+export default function getUpcomingGoldenHours(location: IPosition): IGoldenHours {
+    const todayGoldenHours = getGoldenHours(moment(), location);
+    const tomorrowGoldenHours = getGoldenHours(moment().add(1, 'd'), location);
+
+    const currentInterval = moment().subtract(1, 'h');
+    if (currentInterval.isBefore(todayGoldenHours.morning)) {
+        return {
+            morning: todayGoldenHours.morning,
+            evening: todayGoldenHours.evening,
+        };
+    } else if (currentInterval.isBefore(todayGoldenHours.evening)) {
+        return {
+            morning: tomorrowGoldenHours.morning,
+            evening: todayGoldenHours.evening,
+        }
+    } else {
+        return tomorrowGoldenHours;
+    }
 }
