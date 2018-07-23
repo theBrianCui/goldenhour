ff175756a41b315ab710d146d75beb30d3b16164
Brian Cui <brian.cui@live.com>
2018-07-23 14:28:02 -0700

    location location location

--- src/App.tsx
+++ src/App.tsx
@@ -3,19 +3,46 @@ import './App.css';
 
 import logo from './logo.svg';
 
-class App extends React.Component {
+interface IAppState {
+  location: string;
+}
+
+class App extends React.Component<object, IAppState> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      location: "Determining your location..."
+    };
+  }
+
   public render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello, World!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          {this.state.location}
         </p>
       </div>
     );
+  }
+
+  public componentDidMount() {
+    let location = "Location is not supported by this browser";
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((success) => {
+        console.log("Location received!");
+        location = `${success.coords.latitude} LAT ${success.coords.longitude} LONG`;
+
+        this.setState({
+          location,
+        });
+      }, (err) => {
+        console.log(`Error: ${err.toString()}`);
+      });
+    }
   }
 }
 
